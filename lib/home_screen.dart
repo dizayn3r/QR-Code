54c32a43f0108fef893e348f528ffa29c5fe7739
Shashi Kant <dizayn3r@gmail.com>
2022-09-13 10:57:08 +0530

    qr code scan.dart and generate.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code/code/scan.dart';
+
+import 'code/generate.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +13,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homepage'),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image(
+                image: NetworkImage(
+                    "https://media.istockphoto.com/vectors/qr-code-scan-phone-icon-in-comic-style-scanner-in-smartphone-vector-vector-id1166145556")),
+            flatButton("Scan QR CODE", ScanPage()),
+            SizedBox(
+              height: 20.0,
+            ),
+            flatButton("Generate QR CODE", GeneratePage()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget flatButton(String text, Widget widget) {
+    return ElevatedButton(
+      onPressed: () async {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => widget));
+      },
+      child: Text(text),
+    );
   }
 }
